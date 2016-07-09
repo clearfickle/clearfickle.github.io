e997554647ecd9e326b688652f0f63c542fc0b65
clearfickle <clearfickle@users.noreply.github.com>
2016-07-09 10:05:28 +0800

    content, deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+export GIT_SSH=~/.ssh/clearfickle.sh 
+
+rm -rf public/*
+
+hugo
+cd public
+git add -A
+git cm -m 'rebuild'
+git push origin master
+
+cd ..
+git add public
+git cm -am 'rebuild'
+git push origin dev
